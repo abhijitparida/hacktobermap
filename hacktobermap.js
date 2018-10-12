7160f9854fdc1d1c026a1af39b57a12fdfd7e7ee
Matt <faymatt96@gmail.com>
2018-10-12 07:54:43 -0400

    Add Matt's info

--- hacktobermap.js
+++ hacktobermap.js
@@ -1,3 +1,4 @@
 var hacktobermap = [
   { name: "Abhijit Parida", location: "Bhubaneswar", latitude: 20.296059, longitude: 85.824539 },
+  { name: "Matt Fay", location: "Pennsylvania, USA", latitude: 41.409120, longitude: -75.662390 }, 
 ];
