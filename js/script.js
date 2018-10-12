089c0386115e597080a24c5719b46d4af33bb4c4
Abhijit Parida <abhijitparida.me@gmail.com>
2018-10-12 22:54:25 +0530

    Handle overlapping markers

--- js/script.js
+++ js/script.js
@@ -19,12 +19,30 @@ if (navigator.geolocation) {
 }
 
 // Read user data from hacktobermap.js and render markers
+var markers = L.markerClusterGroup({
+  spiderfyOnMaxZoom: false,
+  showCoverageOnHover: false,
+  zoomToBoundsOnClick: false,
+  spiderLegPolylineOptions: {
+    opacity: 0
+  },
+  spiderfyDistanceMultiplier: 2.5,
+  iconCreateFunction: function(cluster) {
+    return L.divIcon({ html: '<div><span>' + cluster.getChildCount() + ' people</span></div>', className: 'marker-cluster', iconSize: L.point(40, 40)});
+  }
+});
 for (var i = 0; i < hacktobermap.length; i++) {
   var name = hacktobermap[i].name;
   var locality = hacktobermap[i].location;
   var latitude = hacktobermap[i].latitude;
   var longitude = hacktobermap[i].longitude;
-  L.marker([latitude, longitude])
-    .addTo(map)
-    .bindTooltip('<b>' + name + '</b><br>' + locality, {permanent: true, direction: 'right'});
+  var marker = L.marker([latitude, longitude]).bindTooltip('<b>' + name + '</b><br>' + locality, {permanent: true, direction: 'right'});
+  markers.addLayer(marker);
 }
+map.addLayer(markers);
+markers.on('clustermouseover', function (cluster) {
+  cluster.layer.spiderfy();
+});
+markers.on('clustermouseout', function (cluster) {
+  cluster.layer.unspiderfy();
+});
