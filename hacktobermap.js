d9ead72aa832b39c47d9659573ac29c45b30ff2b
rrrahal <rrrahal@gmail.com>
2018-10-12 10:19:39 -0300

    add myself to hacktobermap and change the formatting

--- hacktobermap.js
+++ hacktobermap.js
@@ -1,6 +1,32 @@
 var hacktobermap = [
-  { name: "Abhijit Parida", location: "Bhubaneswar", latitude: 20.296059, longitude: 85.824539 },
-  { name: "Matt Fay", location: "Pennsylvania, USA", latitude: 41.409120, longitude: -75.662390 },
-	{ name: "Jack Thibaut", location: "Barcelona", latitude: 41.385063, longitude: 2.173404 },
-  { name: "Stanislav Lem", location: "Moscow", latitude: 55.755825, longitude: 37.617298 },
+  {
+    name: "Abhijit Parida",
+    location: "Bhubaneswar",
+    latitude: 20.296059,
+    longitude: 85.824539
+  },
+  {
+    name: "Matt Fay",
+    location: "Pennsylvania, USA",
+    latitude: 41.40912,
+    longitude: -75.66239
+  },
+  {
+    name: "Jack Thibaut",
+    location: "Barcelona",
+    latitude: 41.385063,
+    longitude: 2.173404
+  },
+  {
+    name: "Stanislav Lem",
+    location: "Moscow",
+    latitude: 55.755825,
+    longitude: 37.617298
+  },
+  {
+    name: "Rafael Rahal",
+    location: "São Paulo",
+    latitude: -23.5,
+    longitude: -46.63
+  }
 ];
