35128038ac18fe3bdc60fbaa8de7576fe43f8879
rajputvishwas <rajput_vishwas@infosys.com>
2018-10-26 16:41:57 +0530

    Add my location details

--- hacktobermap.js
+++ hacktobermap.js
@@ -833,4 +833,10 @@ var hacktobermap = [
     latitude: "20.296059",
     longitude: "85.824539"
   },
+  {
+    name: "Vishwas S Rajput",
+    location: "Pune, India",
+    latitude: "18.596190",
+    longitude: "73.712790"
+  },
 ];
