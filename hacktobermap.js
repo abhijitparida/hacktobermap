233d246fc0ecaf3cdd0c6365a59026cacc1e9aca
Marcin <mmarcin8@gmail.com>
2018-10-28 21:18:23 +0100

    Added my location to the array

--- hacktobermap.js
+++ hacktobermap.js
@@ -1187,4 +1187,10 @@ var hacktobermap = [
     latitude: 35.570977,
     longitude: -82.552738
   },
+    {
+    name: 'Marcin',
+    location: 'Warsaw, Poland',
+    latitude: 52.185536,
+    longitude: 21.060847,
+  },
 ];
