fd06751ba60397884abb14f0eddabb75c4de6fb9
ravdeepsing <44563741+ravdeepsing@users.noreply.github.com>
2018-10-29 11:39:33 +0530

    Update hacktobermap.js

--- hacktobermap.js
+++ hacktobermap.js
@@ -1187,4 +1187,10 @@ var hacktobermap = [
     latitude: 35.570977,
     longitude: -82.552738
   },
+  {
+    name: "Numerical Analysis",
+    location: "Buenos Aires",
+    latitude: -34.716,
+    longitude: -58.297,
+  },
 ];
