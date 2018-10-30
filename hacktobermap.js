46969c009a97bab24fe1d45880c6765f46262ba5
ozlop <oswaldo.t.lopez@gmail.com>
2018-10-30 12:55:43 -0700

    Add ozlop's location

--- hacktobermap.js
+++ hacktobermap.js
@@ -1361,4 +1361,10 @@ var hacktobermap = [
     latitude: 39.709739,
     longitude: -86.1372963
   },
+  {
+    name: "ozlop",
+    location: "Milpitas, CA",
+    latitude: 37.428272,
+    longitude: -121.906624
+  }
 ];
