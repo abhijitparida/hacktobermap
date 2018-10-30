d689098b0a930cad7a6efffeb47b0fac1fc9ddfe
Steven Chadwick <schad@uw.edu>
2018-10-30 15:27:22 -0700

    adding Seattle

--- hacktobermap.js
+++ hacktobermap.js
@@ -1361,4 +1361,10 @@ var hacktobermap = [
     latitude: 39.709739,
     longitude: -86.1372963
   },
+  {
+    name: "Steven",
+    location: "Seattle, WA, USA",
+    latitude: 47.606,
+    longitude: -122.335
+  }
 ];
