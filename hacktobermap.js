725ca4d3fa84b754fec13785ae39e1302a03cbfc
C. Janzen <cathjanzen@gmail.com>
2018-10-30 22:28:34 -0500

    Add C. Janzen info

--- hacktobermap.js
+++ hacktobermap.js
@@ -1361,4 +1361,10 @@ var hacktobermap = [
     latitude: 39.709739,
     longitude: -86.1372963
   },
+  {
+    name: "C. Janzen",
+    location: "Wichita, KS, USA",
+    latitude: 37.6871761,
+    longitude: -97.330053
+  },
 ];
