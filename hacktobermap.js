654c76fcd7b8eccda9f647b6f1b546030b49326b
FrikanRw <ferwee@gmail.com>
2018-10-31 22:05:20 +0200

    Update hacktobermap.js

--- hacktobermap.js
+++ hacktobermap.js
@@ -1361,4 +1361,10 @@ var hacktobermap = [
     latitude: 39.709739,
     longitude: -86.1372963
   },
+    {
+    name: "FrikanRw",
+    location: "Pretoria",
+    latitude: -25.789392,
+    longitude: 28.341563 
+  }.
 ];
