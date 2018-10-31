d8ecee674344a15eb66b168f945aa20e5a4eaa0f
Brandon Mellus <mellusbrandon@gmail.com>
2018-10-31 19:23:19 -0400

    Add East Lansing to map

--- hacktobermap.js
+++ hacktobermap.js
@@ -1361,4 +1361,10 @@ var hacktobermap = [
     latitude: 39.709739,
     longitude: -86.1372963
   },
+  {
+    name: "Brandon Mellus",
+    location: "East Lansing, Michigan, USA",
+    latitude: 42.746880,
+    longitude: -84.483704
+  }
 ];
