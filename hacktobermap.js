9fefd6d7ae2d683fd02416b557cd94a696ae6035
Pranava Dhar <pranavadhar135@gmail.com>
2018-11-01 07:10:19 +0530

    Update hacktobermap.js

--- hacktobermap.js
+++ hacktobermap.js
@@ -1361,4 +1361,10 @@ var hacktobermap = [
     latitude: 39.709739,
     longitude: -86.1372963
   },
+    {
+    name: "Pranava Dhar",
+    location: "Noida, India",
+    latitude: 28.535,
+    longitude: 77.391 
+  }
 ];
