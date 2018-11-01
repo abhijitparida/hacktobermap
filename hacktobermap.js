983c7ec35e79e2f26f0ebd0a71a068fd59aaf286
Pseudogenesis <samattlevine@yahoo.com>
2018-10-31 18:21:08 -0700

    Added a Fullerton, California dot to the map!

--- hacktobermap.js
+++ hacktobermap.js
@@ -1361,4 +1361,10 @@ var hacktobermap = [
     latitude: 39.709739,
     longitude: -86.1372963
   },
+  {
+    name: "Pseudogenesis",
+    location: "Fullerton, California, USA",
+    latitude: 33.870350,
+    longitude: -117.924300 
+  },
 ];
