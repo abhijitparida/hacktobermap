d6c239fa12a85f4528a727e085d89a59ce5ec7c5
S. Hall <shall.usnavy@gmail.com>
2019-09-27 11:30:36 -0400

    Add TravelingMan

--- hacktobermap.js
+++ hacktobermap.js
@@ -5,4 +5,10 @@ var hacktobermap = [
     latitude: 20.296059,
     longitude: 85.824539
   },
+  {
+    name: "S. Hall",
+    location: "Florida",
+    latitude: 28.53833,
+    longitude: -81.378883
+  }
 ];
