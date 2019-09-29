d7443ddc352c53c8a99f0b291f49fc57fab7cb1f
Sandro Pirtskhalava <sandro1211@gmail.com>
2019-09-29 12:18:51 +0400

    added name sandri pirtskhalava

--- hacktobermap.js
+++ hacktobermap.js
@@ -5,4 +5,10 @@ var hacktobermap = [
     latitude: 20.296059,
     longitude: 85.824539
   },
+    {
+    name: "Sandro Pirtskhalava",
+    location: "Tbilisi,Georgia",
+    latitude: 41.715137,
+    longitude: 44.827095
+  },
 ];
