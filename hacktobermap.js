2b0c5d09d16c7aecb362569faf66c2c34b1b3806
khushbu <khushbug014@gmail.com>
2019-10-01 00:14:23 +0530

    added new location

--- hacktobermap.js
+++ hacktobermap.js
@@ -5,4 +5,10 @@ var hacktobermap = [
     latitude: 20.296059,
     longitude: 85.824539
   },
+  {
+    name: "Khushbu Goyal",
+    location: "India",
+    latitude: 20.593683,
+    longitude: 78.962883
+  },
 ];
