876b6969217b9fbf3e2959753213ebc6dcc2fbb5
Fredrik Engstrand <frewacom@gmail.com>
2019-09-30 10:13:09 +0200

    Added new marker

--- hacktobermap.js
+++ hacktobermap.js
@@ -5,4 +5,10 @@ var hacktobermap = [
     latitude: 20.296059,
     longitude: 85.824539
   },
+  {
+    name: "Fredrik Engstrand",
+    location: "Uppsala",
+    latitude: 59.858562,
+    longitude: 17.638927
+  }
 ];
