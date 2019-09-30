6f213d3a88771bfc1363c55796cb823f46b3a5ec
ComputerWala <shivsoni377@gmail.com>
2019-10-01 01:08:36 +0530

    updated

--- hacktobermap.js
+++ hacktobermap.js
@@ -5,4 +5,10 @@ var hacktobermap = [
     latitude: 20.296059,
     longitude: 85.824539
   },
+   {
+    name: "Aman Kumar Soni",
+    location: "Jalandhar",
+    latitude: 31.3260,
+    longitude: 75.5762
+  },
 ];
