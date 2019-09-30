1555663ca011b4667c205868917c7a171b972361
Renata Miranda <30598706+mirandars@users.noreply.github.com>
2019-09-30 11:41:44 -0300

    Add brazilian participant to HacktoberMap 2019

--- hacktobermap.js
+++ hacktobermap.js
@@ -5,4 +5,11 @@ var hacktobermap = [
     latitude: 20.296059,
     longitude: 85.824539
   },
+  {
+    name: "Renata Miranda",
+    location: "São Paulo, BR",
+    latitude: -23.5489,
+    longitude: -46.6388 
+  },
+  
 ];
