dbd878a79eb8df35fe19f94595dd98579928a865
ajayjha5899 <jha5899@gmail.com>
2019-09-30 18:51:30 +0530

    Added myself to Hacktobermap2019

--- hacktobermap.js
+++ hacktobermap.js
@@ -5,4 +5,18 @@ var hacktobermap = [
     latitude: 20.296059,
     longitude: 85.824539
   },
+  {
+    name: "Ajay Jha",
+    location: "Haryana, India",
+    latitude: 29.96,
+    longitude: 78.87 
+  },
+  /* FORMAT       
+  {
+    name: "Your name",
+    location: "Your Location",
+    latitude: 00.00,
+    longitude: 00.00    
+  }
+  */
 ];
