4f19716a344fdfe97e2b4aef3c1260470d3ebdca
kshitij <kshitijagrawal2607@gmail.com>
2019-09-30 23:46:55 +0530

    Added location :p

--- hacktobermap.js
+++ hacktobermap.js
@@ -5,4 +5,10 @@ var hacktobermap = [
     latitude: 20.296059,
     longitude: 85.824539
   },
+  {
+    name: "Kshitij Agrawal",
+    location: "Kharagpur",
+    latitude: 10.101010,
+    longitude: 20.202020
+  },
 ];
