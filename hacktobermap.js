343b083108a2970d1b630eabb6370326d1210dab
Jerrit <50867655+cherrysrc@users.noreply.github.com>
2019-09-30 17:54:47 +0200

    Update hacktobermap.js

--- hacktobermap.js
+++ hacktobermap.js
@@ -5,4 +5,10 @@ var hacktobermap = [
     latitude: 20.296059,
     longitude: 85.824539
   },
+  {
+    name: "cherrysrc",
+    location: "Lower Saxony",
+    latitude: 52.949300,
+    longitude: 10.050893
+  }
 ];
