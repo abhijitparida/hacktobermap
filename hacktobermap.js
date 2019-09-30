9ffb3499a25aa86e1063f5ad4c3467df1dac9ad1
Braintelligence <Braintelligence@users.noreply.github.com>
2019-09-30 14:27:20 +0200

    Update hacktobermap.js

--- hacktobermap.js
+++ hacktobermap.js
@@ -5,4 +5,10 @@ var hacktobermap = [
     latitude: 20.296059,
     longitude: 85.824539
   },
+  {
+    name: "Braintelligence",
+    location: "Frankfurt",
+    latitude: 50.1211909,
+    longitude: 8.5665249
+  }
 ];
