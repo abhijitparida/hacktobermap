793c51b4e2a352c6a78e3ed5200ae31e7fbcd31c
Zara <zotamias@gmail.com>
2019-10-01 17:45:16 -0400

    Update hacktobermap.js

--- hacktobermap.js
+++ hacktobermap.js
@@ -6,10 +6,10 @@ var hacktobermap = [
     longitude: 85.824539
   },
 
-{
+  {
     name: "Zara Otamias",
     location: "Massachusetts",
     latitude: 42.407211,
     longitude: -71.382439 
-  }
+  },
 ];
