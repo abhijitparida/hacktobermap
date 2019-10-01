9750ea1a6aa807c7cca5d445f0c229ad992ce760
nrjydv98 <56016815+nrjydv98@users.noreply.github.com>
2019-10-01 12:42:01 +0530

    Update hacktobermap.js for 2019

--- hacktobermap.js
+++ hacktobermap.js
@@ -5,4 +5,10 @@ var hacktobermap = [
     latitude: 20.296059,
     longitude: 85.824539
   },
+  {
+    name: "Neeraj Yadav",
+    location: "Himachal Pradesh",
+    latitude: 31.817261,
+    longitude: 77.302757,
+  },
 ];
