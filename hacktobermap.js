a44241efbe545c99e86162cf5a69d3035f856863
Arpit Jain <arpit.jain@iiitdmj.ac.in>
2019-10-01 12:24:24 +0530

    Update hacktobermap.js

--- hacktobermap.js
+++ hacktobermap.js
@@ -5,4 +5,10 @@ var hacktobermap = [
     latitude: 20.296059,
     longitude: 85.824539
   },
+  {
+    name: "Arpit J",
+    location: "Kerela",
+    latitude: 10.84,
+    longitude: 76.28 
+  },
 ];
