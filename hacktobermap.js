84076e6540044ed0b72d64f8d0ae2dfd103764d1
armspkt <49720317+armspkt@users.noreply.github.com>
2019-10-01 10:40:40 +0700

    add my location

--- hacktobermap.js
+++ hacktobermap.js
@@ -5,4 +5,10 @@ var hacktobermap = [
     latitude: 20.296059,
     longitude: 85.824539
   },
+  {
+    name: "Supakorn Thongtra",
+    location: "Bangkok",
+    latitude: 13.756331,
+    longitude: 100.501762
+  },
 ];
