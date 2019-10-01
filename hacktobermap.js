2b1fa862f1725ea50509a4435babf59046f67357
Georgina Hodgkinson <gtl90@hotmail.com>
2019-10-01 20:40:03 +0100

    added bletchley park to map

--- hacktobermap.js
+++ hacktobermap.js
@@ -5,4 +5,10 @@ var hacktobermap = [
     latitude: 20.296059,
     longitude: 85.824539
   },
+  {
+    name: "Girls Code MK",
+    location: "Bletchely park",
+    latitude: 51.9977,
+    longitude: 0.7407
+  }
 ];
