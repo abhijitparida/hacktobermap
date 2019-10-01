f5c74bcaf07dc8e806ec840fc893fa25f1fbe550
Rachel Johnson <rachelktyjohnson@gmail.com>
2019-10-01 14:25:48 +1000

    :hammer: Add Rachel Johnson to map

--- hacktobermap.js
+++ hacktobermap.js
@@ -5,4 +5,10 @@ var hacktobermap = [
     latitude: 20.296059,
     longitude: 85.824539
   },
+  {
+    name: "Rachel Johnson",
+    location: "Sydney, Australia",
+    latitude: -33.8563567,
+    longitude: 151.2116543 
+  }
 ];
