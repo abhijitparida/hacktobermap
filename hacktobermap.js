487bd51dcee8bef5a0ced7ed983f75d3edf19b16
Pradyuman Dixit <crvasu779@gmail.com>
2019-10-01 09:11:12 +0200

    Update hacktobermap.js

--- hacktobermap.js
+++ hacktobermap.js
@@ -5,4 +5,10 @@ var hacktobermap = [
     latitude: 20.296059,
     longitude: 85.824539
   },
+  {
+    name: "Pradyuman Dixit",
+    location: "Hague",
+    latitude: 52.080190,
+    longitude: 4.310130 
+  },
 ];
