883df519dc842bb8dfa995594a4d75de4bbe194d
Eniedson Junior <eniedson.junior@ccc.ufcg.edu.br>
2019-10-01 17:51:55 -0300

    My location

--- hacktobermap.js
+++ hacktobermap.js
@@ -4,5 +4,10 @@ var hacktobermap = [
     location: "Bhubaneswar",
     latitude: 20.296059,
     longitude: 85.824539
+  }, {
+    name: "Eniedson Junior",
+    location: "Brasil",
+    latitude: -7.022664,
+    longitude: -35.856996
   },
 ];
