1fc709e79e8f66d20805017d89c8c00a4e6e3ca6
Ogundiji Bolade <kamponistullar@gmail.com>
2019-10-01 14:13:09 +0100

    Modified my location on the map

--- hacktobermap.js
+++ hacktobermap.js
@@ -5,4 +5,10 @@ var hacktobermap = [
     latitude: 20.296059,
     longitude: 85.824539
   },
+  {
+    name: "Ogundiji Bolade (AdroitCode)",
+    location: "Lagos Nigeria",
+    latitude: 6.649190,
+    longitude: 3.302540 
+  },
 ];
