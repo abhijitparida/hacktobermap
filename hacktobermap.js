fb44721491738a3a0e69b71e2f1675545f5a6166
Calum Dingwall <24cdingwall@gmail.com>
2019-10-01 17:26:12 -0400

    Add me on map

--- hacktobermap.js
+++ hacktobermap.js
@@ -5,4 +5,10 @@ var hacktobermap = [
     latitude: 20.296059,
     longitude: 85.824539
   },
+  {
+    name: "Calum Dingwall",
+    location: "New Jersey, USA",
+    latitude: 40.56965,
+    longitude: -74.6255 
+  },
 ];
