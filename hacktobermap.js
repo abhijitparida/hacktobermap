211ec87ab2f9bb3553587f2a5b9fd515365f883b
Moksh Chaudhary <moksh4445@gmail.com>
2019-10-01 18:36:48 +0530

    Update hacktobermap.js
    Added My own Coordinate !

--- hacktobermap.js
+++ hacktobermap.js
@@ -5,4 +5,9 @@ var hacktobermap = [
     latitude: 20.296059,
     longitude: 85.824539
   },
+  name: "Moksh",
+    location: "Chandigarh",
+    latitude: 30.733315,
+    longitude: 76.779419
+  },
 ];
