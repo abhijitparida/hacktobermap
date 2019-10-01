eb70a0efcb14e253ee58270a0e01e6cbff54bc81
Ian Kahwai <iankahwai@gmail.com>
2019-10-01 15:53:08 +0200

    Add my location to hacktobermap

--- hacktobermap.js
+++ hacktobermap.js
@@ -5,4 +5,10 @@ var hacktobermap = [
     latitude: 20.296059,
     longitude: 85.824539
   },
+   {
+    name: "Ian Kahwai",
+    location: "Harare",
+    latitude: -17.825167,
+    longitude: 31.033510
+  },
 ];
