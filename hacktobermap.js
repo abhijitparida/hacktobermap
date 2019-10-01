11baca18f5d4545f6616dca0c893d892672d13b5
Kleiser Sarifo <kleiser.sarifo@gmail.com>
2019-10-01 14:20:35 +0200

    HBFest-2019

--- hacktobermap.js
+++ hacktobermap.js
@@ -5,4 +5,10 @@ var hacktobermap = [
     latitude: 20.296059,
     longitude: 85.824539
   },
+  {
+    name: "Kleiser Sarifo",
+    location: "Maputo",
+    latitude: -25.8307354,
+    longitude: 32.3082668
+  },
 ];
