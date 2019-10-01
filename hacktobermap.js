ae38692687b5e13edc5fe91fc6693f5786f045f1
jpk3lly <39955081+jpk3lly@users.noreply.github.com>
2019-10-01 11:51:54 +0100

    Update hacktobermap.js

--- hacktobermap.js
+++ hacktobermap.js
@@ -5,4 +5,10 @@ var hacktobermap = [
     latitude: 20.296059,
     longitude: 85.824539
   },
+  {
+    name: "John-Paul Kelly",
+    location: "Glusburn",
+    latitude: 53.009033,
+    longitude: 55.052583
+  },
 ];
