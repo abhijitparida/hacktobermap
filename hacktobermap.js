3c38c1eaca992d0a177fb2cefc077d72253e1a45
dcassoli <dcassoli@gmail.com>
2019-10-01 14:48:45 -0300

    Add myself location

--- hacktobermap.js
+++ hacktobermap.js
@@ -5,4 +5,10 @@ var hacktobermap = [
     latitude: 20.296059,
     longitude: 85.824539
   },
+  {
+    name: "Daniel Cassoli",
+    location: "São Paulo",
+    latitude: -23.5666985,
+    longitude: -46.633308
+  },
 ];
