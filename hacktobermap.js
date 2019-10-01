1cec21049cf6888588d1d408b2909e6b24ea08a5
Adriano Skroch <adriano.skroch@skipthedishes.ca>
2019-09-30 22:30:07 -0500

    add my location to hacktobermap 2019

--- hacktobermap.js
+++ hacktobermap.js
@@ -5,4 +5,10 @@ var hacktobermap = [
     latitude: 20.296059,
     longitude: 85.824539
   },
+  {
+    name: "Adriano Skroch",
+    location: "Winnipeg, MB, CA",
+    latitude: 49.895138,
+    longitude: -97.138374
+  },
 ];
