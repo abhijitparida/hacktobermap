d516ddeccb54fe9943565f0b98f367bcd27b9a2f
Jordan <jordangill600@gmail.com>
2019-10-01 22:43:46 +0100

    Added my location

--- hacktobermap.js
+++ hacktobermap.js
@@ -5,4 +5,10 @@ var hacktobermap = [
     latitude: 20.296059,
     longitude: 85.824539
   },
+  {
+    name: "Jordan",
+    location: "Scotland",
+    latitude: 56.490669,
+    longitude: -4.202646 
+  },
 ];
