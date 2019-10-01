8ea26d341b11523923a7444322ca176e4fc1af7d
hariprasetia <hari.prasetia@gmail.com>
2019-10-01 09:14:25 +0700

    Hacktobermap Hari Prasetia

--- hacktobermap.js
+++ hacktobermap.js
@@ -5,4 +5,10 @@ var hacktobermap = [
     latitude: 20.296059,
     longitude: 85.824539
   },
+  {
+    name: "Hari Prasetia",
+    location: "Jakarta",
+    latitude: -6.229728,
+    longitude: 106.6894312
+  },
 ];
