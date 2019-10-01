ec0854e2529e2a1b1a7890d3ac075270b1557ee0
annacia <carol.inhasz@hotmail.com>
2019-09-30 23:29:13 -0300

    added myself

--- hacktobermap.js
+++ hacktobermap.js
@@ -5,4 +5,10 @@ var hacktobermap = [
     latitude: 20.296059,
     longitude: 85.824539
   },
+  {
+    name: "Anna CIA",
+    location: "São Paulo",
+    latitude: -23.550520,
+    longitude: -46.633308 
+  },
 ];
