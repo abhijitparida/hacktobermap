6eab8a19111252391d92725425a67fdac0b09f7d
sasithorn.sup <sasithorn.sup@kiatnakinbank.co.th>
2019-10-02 00:43:36 +0700

    add my information

--- hacktobermap.js
+++ hacktobermap.js
@@ -4,5 +4,10 @@ var hacktobermap = [
     location: "Bhubaneswar",
     latitude: 20.296059,
     longitude: 85.824539
-  },
+  },{
+    name: "Sasithorn Supamarkpukdee",
+    location: "Thailand",
+    latitude: 15.8700,
+    longitude: 100.9925
+  }
 ];
