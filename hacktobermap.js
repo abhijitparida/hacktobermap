57cbdf18598d2b6dcb6854bed42a71f26132e8e0
Huntlax <56044560+HuntLax@users.noreply.github.com>
2019-10-02 00:35:14 +0530

    Update hacktobermap.js

--- hacktobermap.js
+++ hacktobermap.js
@@ -5,4 +5,10 @@ var hacktobermap = [
     latitude: 20.296059,
     longitude: 85.824539
   },
+  {
+    name: "Nitin Sharma",
+    location: "Delhi",
+    latitude: 80.996852,
+    longitude: 95.754533
+  },
 ];
