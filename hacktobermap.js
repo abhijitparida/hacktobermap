f0dda40d82575b1fba0c51ba544dfcbb01e544f5
Zara <zotamias@gmail.com>
2019-10-01 17:32:46 -0400

    Update hacktobermap.js
    Added my location

--- hacktobermap.js
+++ hacktobermap.js
@@ -5,4 +5,11 @@ var hacktobermap = [
     latitude: 20.296059,
     longitude: 85.824539
   },
+
+{
+    name: "Zara Otamias",
+    location: "Massachusetts",
+    latitude: 42.407211,
+    longitude: -71.382439 
+  }
 ];
