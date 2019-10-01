53f227eaada206276d2da957bc21ff59246fe12b
Holly Maze <hollymaze@gmail.com>
2019-10-01 19:06:31 -0400

    Added my location

--- hacktobermap.js
+++ hacktobermap.js
@@ -5,4 +5,10 @@ var hacktobermap = [
     latitude: 20.296059,
     longitude: 85.824539
   },
+  {
+  	name: "Holly Maze",
+    location: "West Virginia",
+    latitude: 38.920139,
+    longitude: -80.181839
+  },
 ];
