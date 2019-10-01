50eb67bc8562b573d3912309094c7a9f448dd811
maxutil <48533021+maxutil@users.noreply.github.com>
2019-10-01 11:20:53 -0700

    Update hacktobermap.js

--- hacktobermap.js
+++ hacktobermap.js
@@ -5,4 +5,10 @@ var hacktobermap = [
     latitude: 20.296059,
     longitude: 85.824539
   },
+  {
+    name: "Chris Chung",
+    location: "Los Angeles",
+    latitude: 34.064973,
+    longitude: -118.373188
+  },
 ];
