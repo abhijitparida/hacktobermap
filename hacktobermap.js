79c82cdd94ae43ddab63aedd339dc812a4215807
Aayush Rawal <aayushrawal@Aayushs-MacBook-Pro.local>
2019-10-02 02:42:47 +0530

    Added profile

--- hacktobermap.js
+++ hacktobermap.js
@@ -5,4 +5,13 @@ var hacktobermap = [
     latitude: 20.296059,
     longitude: 85.824539
   },
+
+  {
+    name: 'Aayush Rawal',
+    location: 'Chennai',
+    latitude: '13.0827',
+    longitude: '80.2707'
+
+  }
+
 ];
