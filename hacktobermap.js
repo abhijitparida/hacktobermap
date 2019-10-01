dd9d8bab0cd13e3863303f813b1c1ce92e92108a
Braden Shipley <bradenrshipley@gmail.com>
2019-10-01 14:15:47 -0500

    added profile to map

--- hacktobermap.js
+++ hacktobermap.js
@@ -5,4 +5,10 @@ var hacktobermap = [
     latitude: 20.296059,
     longitude: 85.824539
   },
+  {
+    name: "Braden Shipley",
+      location:"Denton, TX",
+      latitude: 33.214840,
+      longitude: -97.133064
+  },
 ];
