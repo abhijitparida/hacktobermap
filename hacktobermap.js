5b6ff3c6004e45dc4fa4135c9bae7a942a8c886c
Ananya <ananyaganesh01@gmail.com>
2019-10-02 00:37:26 +0530

    initial commit- Hacktoberfest

--- hacktobermap.js
+++ hacktobermap.js
@@ -5,4 +5,10 @@ var hacktobermap = [
     latitude: 20.296059,
     longitude: 85.824539
   },
+  {
+    name: "Silver",
+    location: "Palo Alto",
+    latitude: 37.427475,
+    longitude: -122.169716
+  }
 ];
