4614ed14fbb06bb301dcca96d92c9c193dfc0924
Vincent <savras@users.noreply.github.com>
2019-10-01 21:10:40 -0700

    Update hacktobermap.js

--- hacktobermap.js
+++ hacktobermap.js
@@ -5,4 +5,10 @@ var hacktobermap = [
     latitude: 20.296059,
     longitude: 85.824539
   },
+  {
+    name: "Savras",
+    location: "Canberra",
+    latitude: -35.280937,
+    longitude: 149.130005
+  },
 ];
