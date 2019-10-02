4c8276c200441f44be0b954a60d044a362881e25
rogersmykenzie <44181353+rogersmykenzie@users.noreply.github.com>
2019-10-02 09:13:44 -0500

    Update hacktobermap.js

--- hacktobermap.js
+++ hacktobermap.js
@@ -5,4 +5,10 @@ var hacktobermap = [
     latitude: 20.296059,
     longitude: 85.824539
   },
+  {
+    name: "Mykenzie Rogers",
+    location: "Dallas",
+    latitude: 32.776665,
+    longitude: -96.796989
+  }
 ];
