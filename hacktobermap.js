a5208dca6810591d59550a771597f64c03075d49
Prateek Gupta <prateekjpg@gmail.com>
2019-10-02 08:09:30 +0530

    Added my information in hactobermap

--- hacktobermap.js
+++ hacktobermap.js
@@ -5,4 +5,10 @@ var hacktobermap = [
     latitude: 20.296059,
     longitude: 85.824539
   },
+  {
+    name: "Prateek Gupta",
+    location: "Kolkata",
+    latitude: 22.57,
+    longitude: 88.36 
+  },
 ];
