70d04e84c893ef42f2e4e2c4d3e211e99a06c8cd
omgscar <56062694+omgscar@users.noreply.github.com>
2019-10-02 14:39:52 +0530

    update hacktobermap.js

--- hacktobermap.js
+++ hacktobermap.js
@@ -5,4 +5,10 @@ var hacktobermap = [
     latitude: 20.296059,
     longitude: 85.824539
   },
+  {
+    name: "Bhavya Madan",
+    location: "Bhubaneshwar",
+    latitude: 20.296059,
+    longitude: 85.824539
+  },
 ];
