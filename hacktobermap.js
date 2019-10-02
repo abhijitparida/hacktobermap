714f03c5e2ddfa6da5447d33c27bcf27c3a21eec
Yash Kumar Arora <arorayash1998@gmail.com>
2019-10-02 11:52:03 +0530

    My First PR

--- hacktobermap.js
+++ hacktobermap.js
@@ -5,4 +5,10 @@ var hacktobermap = [
     latitude: 20.296059,
     longitude: 85.824539
   },
+  {
+    name: "Yash Kumar Arora",
+    location: "Dehradun",
+    latitude: 30.316496,
+    longitude: 78.032188
+  },
 ];
