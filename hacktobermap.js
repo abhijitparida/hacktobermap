0963d4bbdfec9102c14254a30b1ea8dd4d87912f
McFrappe <obeyfranso@gmail.com>
2019-10-02 11:17:53 +0200

    added name to hacktobermap.js

--- hacktobermap.js
+++ hacktobermap.js
@@ -5,4 +5,12 @@ var hacktobermap = [
     latitude: 20.296059,
     longitude: 85.824539
   },
+
+  {
+    name: "Fahad Jamil",
+    location: "Sweden",
+    latitude: 59.85882,
+    longitude: 17.63889
+  },
+
 ];
