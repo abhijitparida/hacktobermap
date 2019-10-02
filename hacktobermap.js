3f14c273bb2cbfe6a617d1b3901d95c1cf378a25
Juvhej <16434417+juvhej@users.noreply.github.com>
2019-10-02 01:59:07 -0700

    Update hacktobermap.js

--- hacktobermap.js
+++ hacktobermap.js
@@ -5,4 +5,10 @@ var hacktobermap = [
     latitude: 20.296059,
     longitude: 85.824539
   },
+   {
+    name: "juvhej",
+    location: "Stockholm",
+    latitude: 59.32,
+    longitude: 18.07 
+  },
 ];
