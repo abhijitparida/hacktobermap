39372cf8c1deb106f5e9cbd684882ecd4ece5b56
hiteshproy@gmail.com <hiteshproy@gmail.com>
2019-10-02 12:58:01 +0530

    added name

--- hacktobermap.js
+++ hacktobermap.js
@@ -4,5 +4,10 @@ var hacktobermap = [
     location: "Bhubaneswar",
     latitude: 20.296059,
     longitude: 85.824539
-  },
+  },{
+    name: "Hitesh Roy",
+    location: "Nagpur",
+    latitude: 18.520430,
+    longitude: 73.856743
+  }
 ];
