9216bcb101c6a368302fb8b98c04fd590322e37c
everlearner <keshavkabra123@gmail.com>
2019-10-02 11:05:23 +0530

    Added new Coordinates revamping the map

--- hacktobermap.js
+++ hacktobermap.js
@@ -1,8 +1,8 @@
 var hacktobermap = [
   {
-    name: "Abhijit Parida",
-    location: "Bhubaneswar",
-    latitude: 20.296059,
-    longitude: 85.824539
+    name: "Keshav K",
+    location: "Bangalore",
+    latitude: 17.385044,
+    longitude: 78.486671
   },
 ];
