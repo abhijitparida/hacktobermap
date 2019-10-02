b165d3cc9f83db45761f2312c32f0afe04717017
Tarun Jain <tarunjain885@gmail.com>
2019-10-02 11:21:55 +0530

    Update hacktobermap.js

--- hacktobermap.js
+++ hacktobermap.js
@@ -5,4 +5,10 @@ var hacktobermap = [
     latitude: 20.296059,
     longitude: 85.824539
   },
+  {
+    name: "Tarun",
+    location: "Bangalore",
+    latitude: 20.296059,
+    longitude: 85.824539
+  },
 ];
