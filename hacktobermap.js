5782dd64bb325a23d77983c9faf2eef9f8e0ddd3
AnnaH <33048158+annahothai@users.noreply.github.com>
2019-10-01 21:32:59 -0700

    Add my name and locaion

--- hacktobermap.js
+++ hacktobermap.js
@@ -5,4 +5,10 @@ var hacktobermap = [
     latitude: 20.296059,
     longitude: 85.824539
   },
+  {
+    name: "Anna Hothai",
+    location: "Oregon",
+    latitude: 43.8041,
+    longitude: -120.5542
+  },
 ];
