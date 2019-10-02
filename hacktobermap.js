e785482809c9e2b71123d5c08418df716ddbf904
LoserCringe <46325757+LoserCringe@users.noreply.github.com>
2019-10-02 18:20:30 -0400

    Update hacktobermap.js
    the file didnt open in my terminal and no idea how to fix it so i edited this

--- hacktobermap.js
+++ hacktobermap.js
@@ -1,8 +1,8 @@
 var hacktobermap = [
   {
-    name: "Abhijit Parida",
-    location: "Bhubaneswar",
-    latitude: 20.296059,
-    longitude: 85.824539
+    name: "Nathan Lim",
+    location: "Ramsey",
+    latitude: 41.057152,
+    longitude: -74.143959
   },
 ];
