e0c255075be8d4f61ee7c4e96dcd0e6b0aea70ee
mhdrare <mhdfaisalakbar206@gmail.com>
2019-10-03 16:03:37 +0700

    Add my info

--- hacktobermap.js
+++ hacktobermap.js
@@ -5,4 +5,10 @@ var hacktobermap = [
     latitude: 20.296059,
     longitude: 85.824539
   },
+  {
+    name: "M Faisal Akbar",
+    location: "Indonesia",
+    latitude: -6.887750,
+    longitude: 107.664880
+  }
 ];
