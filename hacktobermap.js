0b006193ba2cbcfdcb571755679d1ba4564ac3f5
Aman Jain <aman2.jain@paytm.com>
2019-10-03 14:50:36 +0530

    Update Hacktobermap.js

--- hacktobermap.js
+++ hacktobermap.js
@@ -5,4 +5,12 @@ var hacktobermap = [
     latitude: 20.296059,
     longitude: 85.824539
   },
+                    
+    {
+    name: "Aman Jain",
+    location: "Noida",
+    latitude: 28.535517,
+    longitude: 77.391029
+    },
+                   
 ];
