d5720bdbfbf4561649630a403d1db13cefeb9044
Saurav Saha <contact.srvsaha@gmail.com>
2019-10-03 12:54:50 +0530

    Added the Location Info
    Location Information along with Profile Information are added to be shown in the Map

--- hacktobermap.js
+++ hacktobermap.js
@@ -5,4 +5,10 @@ var hacktobermap = [
     latitude: 20.296059,
     longitude: 85.824539
   },
+  {
+    name: "Saurav Saha",
+    location: "West Bengal, India",
+    latitude: 24.38,
+    longitude: 87.84
+  }, 
 ];
