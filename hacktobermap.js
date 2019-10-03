6c55ddc221066284de4517cc03968fccd091cc3f
marianbeck <marian.beck@pm.me>
2019-10-03 22:26:01 +0200

    added location

--- hacktobermap.js
+++ hacktobermap.js
@@ -1,8 +1,14 @@
 var hacktobermap = [
   {
+    name: "Abhijit Parida",
+    location: "Bhubaneswar",
+    latitude: 20.296059,
+    longitude: 85.824539
+  },
+  {
     name: "Marian",
     location: "Laupheim",
     latitude: 48.206005,
     longitude: 9.864143
-  },
+  }
 ];
