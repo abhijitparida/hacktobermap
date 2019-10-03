a37166699d460ab6ca0e0b972f26bf4276d70d8e
Marco Petreri <42040690+marcopetreri@users.noreply.github.com>
2019-10-03 13:27:28 +0200

    Update hacktobermap.js

--- hacktobermap.js
+++ hacktobermap.js
@@ -5,4 +5,10 @@ var hacktobermap = [
     latitude: 20.296059,
     longitude: 85.824539
   },
+  {
+    name: "Marco Petreri",
+    location: "Rome",
+    latitude: 41.902782,
+    longitude: 12.496365
+  }
 ];
