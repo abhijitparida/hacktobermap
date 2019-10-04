56dc1d67a20d88c9e49771192e7dc6585e0afc60
cholorecto <pocholorecto123@gmail.com>
2019-10-04 10:43:20 +0800

    Hacktoberfest

--- hacktobermap.js
+++ hacktobermap.js
@@ -5,4 +5,10 @@ var hacktobermap = [
     latitude: 20.296059,
     longitude: 85.824539
   },
+  {
+    name: "Pocholo Recto",
+    location: "Davao City",
+    latitude: 7.190708,
+    longitude: 125.455338
+  },
 ];
