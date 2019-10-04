8ddac239bba35bcd22573c9f287026bddd7bf0a3
Rachit Pahwa <pahwarachit8888@gmail.com>
2019-10-05 02:21:36 +0530

    Update hacktobermap.js

--- hacktobermap.js
+++ hacktobermap.js
@@ -5,4 +5,10 @@ var hacktobermap = [
     latitude: 20.296059,
     longitude: 85.824539
   },
+  {
+    name: "Jarvis",
+    location: "India",
+    latitude: 20.5937,
+    longitude: 78.9629
+  },
 ];
