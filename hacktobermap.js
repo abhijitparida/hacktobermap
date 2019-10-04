ef725a838af3f6c47e4dc86e2fd72b7b868184f8
Daniel Duhaylungsod <daniel2town@gmail.com>
2019-10-04 11:14:30 +0800

    Hacktoberfest

--- hacktobermap.js
+++ hacktobermap.js
@@ -5,4 +5,10 @@ var hacktobermap = [
     latitude: 20.296059,
     longitude: 85.824539
   },
+  {
+    name: "Daniel Duhaylungsod",
+    location: "Davao City, Philippines",
+    latitude: 7.190708,
+    longitude: 125.455338
+  },
 ];
