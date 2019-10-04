4abf04d59193c10e0c834f942486ceaf2598bf9b
Terence Chambers <terencechambers91@gmail.com>
2019-10-03 22:29:03 -0400

    updated hacktoberMap location!!!

--- hacktobermap.js
+++ hacktobermap.js
@@ -1,8 +1,8 @@
 var hacktobermap = [
   {
-    name: "Abhijit Parida",
-    location: "Bhubaneswar",
-    latitude: 20.296059,
-    longitude: 85.824539
-  },
+    name: 'TC Sneakerhead',
+    location: 'USA',
+    latitude: 36.778259,
+    longitude: -119.417931
+  }
 ];
