60ca400215a34ed494690ef6662f6c9c7c3a2392
Abhijit Parida <abhijitparida.me@gmail.com>
2019-10-06 10:14:03 +0530

    Fix indentation

--- hacktobermap.js
+++ hacktobermap.js
@@ -4,7 +4,8 @@ var hacktobermap = [
     location: "Bhubaneswar",
     latitude: 20.296059,
     longitude: 85.824539
-  }, {
+  },
+  {
     name: "Eniedson Junior",
     location: "Brasil",
     latitude: -7.022664,
