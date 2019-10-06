fb67fa5e9a1e8e1d047d531739a63aa04715ad08
Michal Král <michal.kral@wunderman.cz>
2019-10-06 19:30:24 +0200

    Added my location

--- hacktobermap.js
+++ hacktobermap.js
@@ -5,4 +5,10 @@ var hacktobermap = [
     latitude: 20.296059,
     longitude: 85.824539
   },
+  {
+    name: "Michal",
+    location: "Prague",
+    latitude: 50.08804,
+    longitude:14.42076
+  }
 ];
