6c475110a23aba52bdafd230792ff2895e9a0a4a
xdidier901 <xdidier901@gmail.com>
2019-10-06 17:18:47 -0700

    Added my city location

--- hacktobermap.js
+++ hacktobermap.js
@@ -376,5 +376,11 @@ var hacktobermap = [
     location: "Prague",
     latitude: 50.08804,
     longitude:14.42076
+  },
+  {
+    name: "Didier Valdez",
+    location: "Hermosillo, Sonora, Mexico",
+    latitude: 29.072968,
+    longitude: -110.955917
   }
 ];
