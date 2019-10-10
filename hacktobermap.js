e417beb6dae1d0f9f51b454edd311b81b804447a
ericksurya <ericksurya@email.com>
2019-10-10 07:32:46 +0700

    hacktoberfest

--- hacktobermap.js
+++ hacktobermap.js
@@ -695,4 +695,10 @@ var hacktobermap = [
     latitude: 35.443707,
     longitude: 139.638031
   },
+  {
+    name: "Erick Surya Dinata",
+    location: "Surabaya, Indonesia",
+    latitude: -7.257472,
+    longitude: 112.752090
+  }
 ];
