02a75e3f40765a40d29c154749e3bbfe8e0f7cac
Abhijit Parida <abhijitparida.me@gmail.com>
2019-10-10 09:00:37 +0530

    Fix

--- hacktobermap.js
+++ hacktobermap.js
@@ -1,5 +1,11 @@
 var hacktobermap = [
   {
+    name: "Abhijit Parida",
+    location: "Bhubaneswar",
+    latitude: 20.296059,
+    longitude: 85.824539
+  },
+  {
     name: "Keshav K",
     location: "Bangalore",
     latitude: 17.385044,
